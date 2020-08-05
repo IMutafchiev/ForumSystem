2dab6ae870fc1aece201bf94125b6898edcd546f
IMutafchiev <i.mutafchiev94@gmail.com>
2020-08-05 09:01:22 +0300

    Forum System

---
+++ ForumSystem.App/Areas/Admin/Controllers/TopicsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForumSystem.App.Areas.Admin.Services.Interfaces;
+using ForumSystem.App.Areas.Admin.ViewModels.Topics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumSystem.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[Area]/[Controller]/[Action]")]
+    [Authorize]
+    public class TopicsController : Controller
+    {
+
+        private readonly IAdminTopicService _service;
+
+        public TopicsController(IAdminTopicService service)
+        {
+            _service = service;
+        }
+
+        [Route("/Topics/Create")]
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [Route("/Topics/Create")]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTopicBindingModel model)
+        {
+            await _service.CreateTopicAsync(model);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var topic = await _service.GetTopicAsync(id);
+
+            var viewModel = new DetailsTopicViewModel
+            {
+                Title = topic.Title,
+                Content = topic.Content,
+                AuthorUserName = topic.Author.UserName,
+                CreatedOn = topic.CreatedOn,
+                Posts = topic.Posts.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = new EditTopicBindingModel
+            {
+                Id = id
+            };
+
+            return this.View(model);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTopicBindingModel model)
+        {
+            var id = int.Parse(HttpContext.Request.Query["id"].ToString().Split().ToArray()[0]);
+
+            await _service.EditTopicAsync(id, model);
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ForumSystem.App/Startup.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ForumSystem.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using ForumSystem.App.Services.SendGrid;
+using ForumSystem.App.Seeder;
+using ForumSystem.App.Common;
+using ForumSystem.App.Areas.Admin.Services.Interfaces;
+using ForumSystem.App.Areas.Admin.Services;
+using ForumSystem.Models;
+
+namespace ForumSystem.App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ForumSystemDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ForumSystemDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddScoped<IAdminTopicService, AdminTopicService>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password = new PasswordOptions
+                {
+                    RequireDigit = true,
+                    RequiredLength = 6,
+                    RequireUppercase = true,
+                    RequireLowercase = true,
+                    RequiredUniqueChars = 0,
+                    RequireNonAlphanumeric = false
+                };
+
+
+
+                options.SignIn.RequireConfirmedEmail = true;
+                options.User.RequireUniqueEmail = true;
+
+
+            });
+            services.AddSingleton<ISendGridEmailSender, SendGridEmailSender>();
+
+            services.Configure<SendGridOptions>(this.Configuration.GetSection("EmailSender"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ForumSystemDbContext>();
+                dbContext.Database.Migrate();
+                new RoleSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
+            }
+
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseDatabaseErrorPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+
+                endpoints.MapAreaControllerRoute(
+                    name: "area", GlobalConstants.AdminstrationRoleName,
+                    pattern: "{area:exists}/{cotroller}/{action=Index}/{id?}");
+
+            });
+        }
+    }
+}
